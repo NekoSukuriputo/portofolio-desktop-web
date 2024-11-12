1f6992b7a223b6b2654a79dfad2b00b3f9b86ea0
Abdul Rahmat <abdulrahmat97dev@gmail.com>
2024-11-12 10:43:19 +0700

    adding required component

---
+++ src/components/common/LockWindow.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LockWindow() {
+  return (
+    <div>LockWindow</div>
+  )
+}
---
+++ src/components/common/SleepWindow.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SleepWindow() {
+  return (
+    <div>SleepWindow</div>
+  )
+}
